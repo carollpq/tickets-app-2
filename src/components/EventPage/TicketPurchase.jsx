19c01d6917dc0c3a7947588e16002d87c5d99176
carollpq <107604758+carollpq@users.noreply.github.com>
2024-07-21 08:58:40 +0100

    Add 'Send to Email' Button

--- src/components/EventPage/TicketPurchase.jsx
+++ src/components/EventPage/TicketPurchase.jsx
@@ -1,31 +1,37 @@
-import React, {useState} from 'react';
-import style from './EventPage.module.css';
+import React, { useState } from "react";
+import style from "./EventPage.module.css";
 
-const TicketPurchase = ({price}) => {
-
+const TicketPurchase = ({ price }) => {
   const [isTicketVisible, setTicketVisible] = useState(false);
 
   const handleBuy = () => {
     setTicketVisible(!isTicketVisible); // Show the ticket
-
   };
 
   return (
     <>
-        <div className={style['ticket-purchase-container']}>
-            <h2 className={style['price']}>{price}</h2>
-            <button onClick={handleBuy} className={style['buy-button']}>Book Now</button>
-        </div>
-        {isTicketVisible && (
-         <>
-         <div className={style['overlay']}></div>
-         <div className={style['ticket-popup']}>
-           <h3>Ticket Confirmation</h3>
-           <p>Price: {price}</p>
-           <p>Thank you for your purchase!</p>
-           <button id={style['close-btn']} onClick={handleBuy}>Close</button>
-         </div>
-       </>
+      <div className={style["ticket-purchase-container"]}>
+        <h2 className={style["price"]}>{price}</h2>
+        <button onClick={handleBuy} className={style["buy-button"]}>
+          Book Now
+        </button>
+      </div>
+      {isTicketVisible && (
+        <>
+          <div className={style["overlay"]}></div>
+          <div className={style["ticket-popup"]}>
+            <h3>Ticket Confirmation</h3>
+            <p>Price: {price}</p>
+            <p>Thank you for your purchase!</p>
+            <img src="src/assets/qr_code.png" alt="" id={style["qr-code"]} />
+            <div className={style["btn-container"]}>
+              <button className={style['btn']} onClick={handleBuy}>
+                Close
+              </button>
+              <button className={style['btn']}>Send to Email</button>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
